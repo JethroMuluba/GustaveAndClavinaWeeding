4ab6e6acc350e7a75188f00b3bbba2208ee8275e
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-21 00:45:06 +0100

    Add new guests "Claudie Ndaya" and "Couple Maseko" with their QR codes and table assignments in page.tsx and [slug]/page.tsx to enhance guest representation for the wedding event.

--- src/app/guest/[slug]/page.tsx
+++ src/app/guest/[slug]/page.tsx
@@ -1131,6 +1131,22 @@ const blogPosts = [
     guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752453028/Weeding%20Ya%20Djo/Card/Table%20Baserons/Bottom_guest_Miss_Karl_boqbhb.png",
     guestTable: "Pamela"
   },
+
+  {
+    id: "142",
+    guestName: "Claudie Ndaya",
+    guestQRCode: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752284016/Weeding%20Ya%20Djo/Location_QR_Code_j1afr2.png",
+    guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752453028/Weeding%20Ya%20Djo/Card/Table%20Baserons/Bottom_guest_Miss_Karl_boqbhb.png",
+    guestTable: "Faith"
+  },
+
+  {
+    id: "143",
+    guestName: "Couple Maseko",
+    guestQRCode: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752284016/Weeding%20Ya%20Djo/Location_QR_Code_j1afr2.png",
+    guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752453028/Weeding%20Ya%20Djo/Card/Table%20Baserons/Bottom_guest_Miss_Karl_boqbhb.png",
+    guestTable: "Merah"
+  },
 ]
 
 // Données simulées pour les articles connexes Drognet kinkela
