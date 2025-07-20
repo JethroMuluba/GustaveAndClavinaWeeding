7c080970ad02b6158053a66395258f6a30ed11d0
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-20 14:45:28 +0100

    Update guest names in page.tsx and [slug]/page.tsx, changing "Dianeline" to "Couple Mambu" and "Blessing Samba" to "Couple Muanda David", along with their respective table assignments, to reflect accurate guest information for the wedding event.

--- src/app/guest/page.tsx
+++ src/app/guest/page.tsx
@@ -576,7 +576,7 @@ const tableOfGuest = [
   },
   {
     id: "71",
-    guestName: "Dianeline",
+    guestName: "Couple Mambu",
     guestQRCode: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752284016/Weeding%20Ya%20Djo/Location_QR_Code_j1afr2.png",
     guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752453028/Weeding%20Ya%20Djo/Card/Table%20Baserons/Bottom_guest_Miss_Karl_boqbhb.png",
     guestTable: "Vainqueur"
