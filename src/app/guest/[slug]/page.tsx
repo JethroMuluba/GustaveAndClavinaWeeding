8a89f7ac47d773f55ad82127173b6fcf7dfc8f0d
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-23 23:55:37 +0100

    Update guest list in page.tsx and [slug]/page.tsx by changing "Tehilla" to "Inaya-Sun" and adding new guests "Elie Tshibambe" and "Naomie Shungu" with their respective QR codes and table assignments for improved representation at the wedding event.

--- src/app/guest/[slug]/page.tsx
+++ src/app/guest/[slug]/page.tsx
@@ -1533,8 +1533,25 @@ const blogPosts = [
     guestName: "Couple Ange Kabamba",
     guestQRCode: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752115767/Weeding%20Baka/qr-code_1_dzs5lg.png",
     guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752226871/Weeding%20Baka/Guest-Done/Guest_Blessing_K_eh6uzi.png",
-    guestTable: "Tehilla"
+    guestTable: "Inaya-Sun"
   },
+
+  {
+    id: "192",
+    guestName: "Elie Tshibambe",
+    guestQRCode: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752115767/Weeding%20Baka/qr-code_1_dzs5lg.png",
+    guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752226871/Weeding%20Baka/Guest-Done/Guest_Blessing_K_eh6uzi.png",
+    guestTable: "LindSay"
+  },
+
+  {
+    id: "193",
+    guestName: "Naomie Shungu",
+    guestQRCode: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752115767/Weeding%20Baka/qr-code_1_dzs5lg.png",
+    guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752226871/Weeding%20Baka/Guest-Done/Guest_Blessing_K_eh6uzi.png",
+    guestTable: "Rev Kim"
+  },
+
 
 ]
 
