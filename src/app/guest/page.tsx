463ae4a44a4b2e297a9fca71440e0ae1c6e826ec
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-23 23:12:48 +0100

    Add guest Couple Ange Kabamba

--- src/app/guest/page.tsx
+++ src/app/guest/page.tsx
@@ -1533,6 +1533,15 @@ const tableOfGuest = [
     guestTable: "Tehilla"
   },
 
+  {
+    id: "191",
+    guestName: "Couple Ange Kabamba",
+    guestQRCode: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752115767/Weeding%20Baka/qr-code_1_dzs5lg.png",
+    guestCard: "https://res.cloudinary.com/dr8ofciki/image/upload/v1752226871/Weeding%20Baka/Guest-Done/Guest_Blessing_K_eh6uzi.png",
+    guestTable: "Tehilla"
+  },
+
+
 
 ]
 
